e96ddcc94ccaadecf6fae9891d5a22ec41525e70
Jonathan-X-Bananaman <xuj4454@my.sduhsd.net>
2026-04-22 21:14:56 +0000

    made card more accurate to hi-fi design and tried adding the categories as array

--- team-app/src/ClubList.jsx
+++ team-app/src/ClubList.jsx
@@ -1,12 +1,20 @@
 import './Clublist.css'
+import {usestate} from 'react'
 function ClubList({name,description,categories,time,location}) {
   return (
     <>
       <h1>{name}</h1>
       <h1 class="clublisttimeandplace">{time} {location}</h1>
       <i class="heart"></i>
-      <h3>{categories}</h3>
-      <p>{description}</p>
+      {categories.map((item,index)=>(
+        <div key={item} className="card">
+          <h3>{item.title}</h3>
+          <p>{item.content}</p>
+        </div>
+      ))}
+      <div id="clubdesc">
+        <p>{description}</p>
+      </div>
     </>
   )
 }
