fe758b5e37530be8059936383244bb8c5f708347
Jonathan-X-Bananaman <xuj4454@my.sduhsd.net>
2026-04-27 19:53:03 +0000

    added card base details

--- src/Discover.jsx
+++ src/Discover.jsx
@@ -1,10 +1,12 @@
-
 import ClubList from './ClubList.jsx'
+import './Discover.css'
 function Discover(){
     return(
         <div>
-            <ClubList name="A Club" description="We draw As" categories="fun" time="never" location="CCA"/>
-            <ClubList name="B Club" description="We draw Bs" categories="fun" time="never" location="CCA"/>
+            <div id="clubs">
+                <ClubList name="A Club" description="We draw As" categories="fun" time="never" location="CCA"/>
+                <ClubList name="B Club" description="We draw Bs" categories="fun" time="never" location="CCA"/>
+            </div>
         </div>
     )
 }
