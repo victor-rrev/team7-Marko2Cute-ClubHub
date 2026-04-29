eba3234fd20cbe7411dd005a4341fda8dbf80acf
Jonathan-X-Bananaman <xuj4454@my.sduhsd.net>
2026-04-29 20:57:51 +0000

    started making the events page

--- src/Events.jsx
+++ src/Events.jsx
@@ -1,9 +1,18 @@
+import './Events.css'
+import ClubList from './ClubList.jsx'
 function Events(){
-    return (
+    return(
         <div>
-            <h1>Events Page</h1>
+            <div className="clubEvents">
+                <ClubList name="Cookie Sale" description="We sell cookies" categories="fun" time="never" location="CCA"/>
+                <ClubList name="ASB Dance" description="Go Dancing" categories="fun" time="11:00PM April 1st" location="Walmart"/>
+            </div>
+            <div className="sportsEvents">
+                <ClubList name="Baseball tryouts" description="tryout to join baseball team" categories="fun" time="yesterday" location="PTMS Quad"/>
+                <ClubList name="B Club" description="We draw Bs" categories="fun" time="never" location="CCA"/>
+            </div>
         </div>
     )
-}  
+}
 
 export default Events;
